0cded749c22fdbd8faa0464b8c6966260de0f71a
ofekdov <ofekofek2002@gmail.com>
2023-03-03 20:49:01 +0200

    added query string

--- services/util.service.js
+++ services/util.service.js
@@ -6,7 +6,10 @@ export const utilService = {
     getRandomIntInclusive,
     loadFromStorage,
     saveToStorage,
-    animateCSS
+    animateCSS,
+    setQueryParams,
+    deleteQueryParam,
+    getValFromParam,
 }
 
 function makeId(length = 6) {
@@ -45,6 +48,33 @@ function loadFromStorage(key) {
     return (data) ? JSON.parse(data) : undefined
 }
 
+function setQueryParams(newParams) {
+    const url = new URL(window.location.href);
+    const params = new URLSearchParams(url.search);
+
+    for (var paramName in newParams) {
+        const paramValue = newParams[paramName];
+        params.set(paramName, paramValue); // used to update an existing query string parameter or add a new one if it doesn't exist.
+    }
+
+    url.search = params.toString();
+    window.history.pushState({ path: url.href }, '', url.href); //modify the URL of the current page without reloading the page
+}
+
+function deleteQueryParam(key) {
+    const url = new URL(window.location.href);
+    const params = new URLSearchParams(url.search);
+
+    params.delete(key);
+    url.search = params.toString();
+
+    window.history.pushState({ path: url.href }, '', url.href);
+}
+
+function getValFromParam(key) {
+    const queryStringParams = new URLSearchParams(window.location.search);
+    return queryStringParams.get(key);
+}
 
 function animateCSS(el, animation) {
     const prefix = 'animate__'
